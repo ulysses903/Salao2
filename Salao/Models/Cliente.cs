4a274dd0581093990e4c4e7cf1078f07bb94b57d
ulysses903 <ulysses903@gmail.com>
2019-08-18 15:22:09 -0300

    PhoneNumber

--- Salao/Models/Cliente.cs
+++ Salao/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,16 +9,26 @@ namespace Salao.Models
     public class Cliente
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "Nome")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "Data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
-        public int PhoneNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
         
+
 
         public Cliente()
         {
         }
 
-        public Cliente(int id, string name, DateTime birthDate, int phoneNumber)
+        public Cliente(int id, string name, DateTime birthDate, string phoneNumber)
         {
             Id = id;
             Name = name;
